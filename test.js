bb1b2e315d5415875ecd3d9f7cb33a7105e938f6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-05-27 13:32:54 +0200

    Test addon linking

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const { paths } = require('./test/helpers')
 
 const fixtures = path.resolve(__dirname, 'test', 'fixtures')
 
-test('app, darwin-arm64', async (t) => {
+test('basic, darwin-arm64', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -31,7 +31,7 @@ test('app, darwin-arm64', async (t) => {
   )
 })
 
-test('app, darwin-arm64, preflight', async (t) => {
+test('basic, darwin-arm64, preflight', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -61,7 +61,7 @@ test('app, darwin-arm64, preflight', async (t) => {
   )
 })
 
-test('app, darwin-arm64, package', { skip: !isMac }, async (t) => {
+test('basic, darwin-arm64, package', { skip: !isMac }, async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -77,7 +77,7 @@ test('app, darwin-arm64, package', { skip: !isMac }, async (t) => {
   t.alike(result.slice(-1), paths(['My App.pkg']))
 })
 
-test('app, ios-arm64', async (t) => {
+test('basic, ios-arm64', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -102,7 +102,7 @@ test('app, ios-arm64', async (t) => {
   )
 })
 
-test('app, ios-arm64, preflight', async (t) => {
+test('basic, ios-arm64, preflight', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -132,7 +132,7 @@ test('app, ios-arm64, preflight', async (t) => {
   )
 })
 
-test('app, ios-arm64, package', { skip: !isMac }, async (t) => {
+test('basic, ios-arm64, package', { skip: !isMac }, async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -148,7 +148,7 @@ test('app, ios-arm64, package', { skip: !isMac }, async (t) => {
   t.alike(result.slice(-1), paths(['My App.pkg']))
 })
 
-test('app, linux-arm64', async (t) => {
+test('basic, linux-arm64', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -172,7 +172,7 @@ test('app, linux-arm64', async (t) => {
   )
 })
 
-test('app, linux-arm64, preflight', async (t) => {
+test('basic, linux-arm64, preflight', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -201,7 +201,7 @@ test('app, linux-arm64, preflight', async (t) => {
   )
 })
 
-test('app, linux-arm64, package', { skip: !isLinux }, async (t) => {
+test('basic, linux-arm64, package', { skip: !isLinux }, async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -217,7 +217,7 @@ test('app, linux-arm64, package', { skip: !isLinux }, async (t) => {
   t.alike(result.slice(-1), paths(['My App.AppImage']))
 })
 
-test('app, win32-arm64', async (t) => {
+test('basic, win32-arm64', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -241,7 +241,7 @@ test('app, win32-arm64', async (t) => {
   )
 })
 
-test('app, win32-arm64, preflight', async (t) => {
+test('basic, win32-arm64, preflight', async (t) => {
   const out = await t.tmp()
   const result = []
 
@@ -263,6 +263,112 @@ test('app, win32-arm64, preflight', async (t) => {
       'My App/App/My App.exe',
       'My App/Resources/app.bundle',
       'My App/Resources/preflight.bundle',
+      'My App/AppxManifest.xml',
+      'My App/Assets/My App.png',
+      'My App'
+    ])
+  )
+})
+
+test('addon, darwin-arm64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of build(path.join(fixtures, 'addon', 'app.js'), {
+    out,
+    base: path.join(fixtures, 'addon'),
+    hosts: ['darwin-arm64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(
+    result,
+    paths([
+      'My App.app/Contents/Frameworks/bare-os.3.9.1.framework/Versions/A/bare-os.3.9.1',
+      'My App.app/Contents/Frameworks/bare-os.3.9.1.framework/Versions/A/Resources/Info.plist',
+      'My App.app/Contents/Frameworks/bare-os.3.9.1.framework',
+      'My App.app/Contents/MacOS/My App',
+      'My App.app/Contents/Info.plist',
+      'My App.app/Contents/PkgInfo',
+      'My App.app/Contents/Resources/app.bundle',
+      'My App.app/Contents/Resources/app.png',
+      'My App.app'
+    ])
+  )
+})
+
+test('addon, ios-arm64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of build(path.join(fixtures, 'addon', 'app.js'), {
+    out,
+    base: path.join(fixtures, 'addon'),
+    hosts: ['ios-arm64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(
+    result,
+    paths([
+      'My App.app/Frameworks/bare-os.3.9.1.framework/bare-os.3.9.1',
+      'My App.app/Frameworks/bare-os.3.9.1.framework/Info.plist',
+      'My App.app/Frameworks/bare-os.3.9.1.framework',
+      'My App.app/My App',
+      'My App.app/Info.plist',
+      'My App.app/PkgInfo',
+      'My App.app/app.bundle',
+      'My App.app/app.png',
+      'My App.app'
+    ])
+  )
+})
+
+test('addon, linux-arm64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of build(path.join(fixtures, 'addon', 'app.js'), {
+    out,
+    base: path.join(fixtures, 'addon'),
+    hosts: ['linux-arm64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(
+    result,
+    paths([
+      'My App.AppDir/usr/lib/libbare-os.3.9.1.so',
+      'My App.AppDir/usr/bin/my-app',
+      'My App.AppDir/usr/share/applications/My App.desktop',
+      'My App.AppDir/usr/share/my-app/app.bundle',
+      'My App.AppDir/usr/share/icons/My App.png',
+      'My App.AppDir'
+    ])
+  )
+})
+
+test('addon, win32-arm64', async (t) => {
+  const out = await t.tmp()
+  const result = []
+
+  for await (const resource of build(path.join(fixtures, 'addon', 'app.js'), {
+    out,
+    base: path.join(fixtures, 'addon'),
+    hosts: ['win32-arm64']
+  })) {
+    result.push(path.relative(out, resource))
+  }
+
+  t.alike(
+    result,
+    paths([
+      'My App/App/bare-os-3.9.1.dll',
+      'My App/App/My App.exe',
+      'My App/Resources/app.bundle',
       'My App/AppxManifest.xml',
       'My App/Assets/My App.png',
       'My App'
